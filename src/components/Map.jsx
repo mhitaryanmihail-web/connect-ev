3487f2c03484c130e7ea043991112bda31d6777c
sergey.mkhitaryan <Serge.Mkhitaryan@x5.ru>
2025-12-02 19:17:20 +0300

    fix

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,4 +1,4 @@
-import {MapContainer, TileLayer, useMap} from "react-leaflet";
+import {MapContainer, TileLayer} from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
 import {MapControls} from "./mapControls";
 
