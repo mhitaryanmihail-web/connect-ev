bf0337f156f78e434836af288f50a372e4f6ecce
sergey.mkhitaryan <Serge.Mkhitaryan@x5.ru>
2025-12-02 18:22:43 +0300

    map added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
+import {Map} from "./components/Map";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app-container">
+            <Map/>
+            <div className={"ui-container"}>
+                <div style={{width:'100%', display:'flex', justifyContent:'center', alignItems: 'center'}}>
+                    <Typography>
+                        Connect EV
+                    </Typography>
+                </div>
+                <Button sx={{background: 'white', color: 'black',  pointerEvents: 'auto'}}>
+                    123123
+                </Button>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Map.jsx
@@ -0,0 +1,21 @@
+import {MapContainer, TileLayer} from "react-leaflet";
+import 'leaflet/dist/leaflet.css';
+
+export const Map = () => {
+
+    return <MapContainer
+        center={[55.7558, 37.6173]} // Москва
+        zoom={13}
+        style={{
+            height: '100%', width: '100%',
+            position: 'absolute',
+            zIndex: 1
+        }}
+    >
+        {/* Слой карты OpenStreetMap */}
+        <TileLayer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+    </MapContainer>
+}
